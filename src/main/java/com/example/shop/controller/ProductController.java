7b4b2411e882636cedc64202ccd3c6bc7a09b02b
hakhyun98 <xorjsrla123@naver.com>
2023-09-13 13:33:57 +0900

    20230913

--- src/main/java/com/example/shop/controller/ProductController.java
+++ src/main/java/com/example/shop/controller/ProductController.java
@@ -7,9 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ProductController {
 
-    @GetMapping("/home")
+//    @GetMapping("/home")
     public String listProducts(Model model) {
         // 여기에 제품 목록을 가져오는 코드를 추가할 수 있습니다.
-        return "product/index"; // Thymeleaf 템플릿의 이름
+//        return "product/index"; // Thymeleaf 템플릿의 이름
+        return null;
     }
 }
