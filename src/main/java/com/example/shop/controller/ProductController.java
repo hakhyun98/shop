d47a4bda07db0ff1a8179baebe92df2c4b935a1b
yechan42 <parkyc6627@gmail.com>
2023-09-13 10:27:23 +0900

    Initial commit

---
+++ src/main/java/com/example/shop/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.example.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductController {
+
+    @GetMapping("/products")
+    public String listProducts(Model model) {
+        // 여기에 제품 목록을 가져오는 코드를 추가할 수 있습니다.
+        return "product/list"; // Thymeleaf 템플릿의 이름
+    }
+}
