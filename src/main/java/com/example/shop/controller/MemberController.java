8fd01521dd39c937896e691e57be0a1d5b71b089
hakhyun98 <xorjsrla123@naver.com>
2023-09-20 11:38:12 +0900

    20230920

--- src/main/java/com/example/shop/controller/MemberController.java
+++ src/main/java/com/example/shop/controller/MemberController.java
@@ -1,12 +1,26 @@
 package com.example.shop.controller;
 
+import com.example.shop.domain.Member;
+import com.example.shop.service.MemberService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/member")
 public class MemberController {
-    public class register{
+    MemberService memberService;
+    @GetMapping("/sign-up")
+    public String signupform(Model model) {
+        model.addAttribute("member", Member.builder().build());
+        return "/member/sign-up-form";
+    }
 
+    @PostMapping("/create")
+    public String create(@ModelAttribute("member") Member member, Model model){
+        if(memberService.create(member)>0)
+            return "/home/index";
+        else
+            return "member/sign-up-form";
     }
 }
